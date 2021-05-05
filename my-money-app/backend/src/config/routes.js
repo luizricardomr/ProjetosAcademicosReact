f38ca0b128c932b7d629369ba4c6a9571a7aa0f5
Luiz Ricardo Marinho do Rego <ricardo2006rj@gmail.com>
2021-05-05 18:22:04 -0300

    Adicionando autenticação ao projeto

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server){
+module.exports = function (server) {
+    /*  Rotas protegidas por Token JWT  */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
-    //Definir URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
+    protectedApi.use(auth)
 
-    //Mapeamento das rotas de Ciclo de pagamento
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
-}
+    BillingCycle.register(protectedApi, '/billingCycles')
 
+    /* Rotas abertas  */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+}
